2035997fe04387bc9bac29281787190659f2e2c9
suhgyusgfyuvgyugtruybhxyigh7i <44337427+suhgyusgfyuvgyugtruybhxyigh7i@users.noreply.github.com>
2018-10-22 22:04:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -797,4 +797,164 @@ client.on('message',message =>{
   });
 
 
+client.on('message',async message => {
+
+    if(message.content.startsWith(prefix + "js")) {
+
+  if(!message.channel.guild) return message.reply(' ');
+
+    let rank = message.guild.member(message.author).roles.find('name', '• Alpha » Help');
+
+    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة سبورت لأستخدام هذا الأمر.**');
+
+    let jscodes = message.guild.channels.find(`name`, "✻-discord-js");
+
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
+
+      let filter = m => m.author.id === message.author.id;
+
+      let thisMessage;
+
+      let thisFalse;
+
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+
+  
+
+      message.channel.awaitMessages(filter, {
+
+        max: 1,
+
+        time: 90000,
+
+        errors: ['time']
+
+      })
+
+      .then(collected => {
+
+        collected.first().delete();
+
+        thisMessage = collected.first().content;
+
+        let boi;
+
+        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
+
+  
+
+            message.channel.awaitMessages(filter, {
+
+              max: 1,
+
+              time: 90000,
+
+              errors: ['time']
+
+            })
+
+            .then(collected => {
+
+              collected.first().delete();
+
+              boi = collected.first().content;
+
+              let boi2;
+
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+
+  
+
+                message.channel.awaitMessages(filter, {
+
+                  max: 1,
+
+                  time: 90000,
+
+                  errors: ['time']
+
+                })
+
+                .then(collected => {
+
+                  collected.first().delete();
+
+                boi2 = collected.first().content;
+
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+
+          max: 1,
+
+          time: 90000,
+
+          errors: ['time']
+
+        })
+
+        .then(collected => {
+
+          if(collected.first().content === 'لا') {
+
+            msg.delete();
+
+            message.delete();
+
+            thisFalse = false;
+
+          }
+
+          if(collected.first().content === 'نعم') {
+
+            if(thisFalse === false) return;
+
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+
+            collected.first().delete();
+
+ @everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**Code Alpha©  :arrow_down:** 
+\`\`\`js
+${thisMessage}\`\`\`
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`).then(ping => {
+
+                  ping.react("✅")
+
+                  ping.react("❌")
+
+                })
+
+
+          }
+
+        }
+
+    );
+
+});
+
+      });
+
+    }
+
+      );
+
+    });
+
+}
+
+);
+
+      })}});
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
